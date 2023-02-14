629a846649d197550a9a8b629dafb0ef0f4e37e0
Michael Paquette <jotha885@gmail.com>
2023-02-14 15:27:02 +0100

    really temporary lsd inclusion in 14.sh

--- 14_diff_run_odf.sh
+++ 14_diff_run_odf.sh
@@ -3,122 +3,71 @@
 # Load Local Variables
 source ./SET_VARIABLES.sh
 
-python3 ${SCRIPTS}/round_bvals.py \
-    --in ${DIFF_DATA_NORM_RELEASE_DIR}/data_norm.bval \
-    --out ${DIFF_DATA_NORM_RELEASE_DIR}/data_norm_rounded.bval
 
-echo 'Fit CSA odf'
-python3 ${SCRIPTS}/fit_csa.py \
-    ${DIFF_DATA_NORM_RELEASE_DIR}/data_norm.nii.gz \
-    ${DIFF_DATA_NORM_RELEASE_DIR}/data_norm_rounded.bval \
-    ${DIFF_DATA_NORM_RELEASE_DIR}/data_norm.bvec \
-    ${DIFF_DATA_DIR}/mask.nii.gz \
-    ${ODF_DIR}/csa.nii.gz \
-    ${N_CORES} 1e-5 0.006 6
+# make LSD processing folder
+echo "Setting up files for LSD processing"
+LSD_DIR=${DIFF_DIR}'/lsd_reprocess/'
+mkdir ${LSD_DIR}
+
+# obviously this need a compact version liviing in scripts
+python /data/pt_02101_dMRI/data/001_C_W_EMMA/preprocessed/20210127_151556_001_C_W_EMMA_TAI_E_1_1_rr/scripts/round_bvals.py --in ${DIFF_DATA_RELEASE_DIR}'/data.bval' --out ${LSD_DIR}'/rounded.bval'
+python /data/hu_paquette/tools/scilpy/scripts/scil_flip_gradients.py ${DIFF_DATA_RELEASE_DIR}'/data.bvec' ${LSD_DIR}'/flipX.bvec' x --fsl
 
 
-echo 'Sharpen odf'
-mkdir -p ${ODF_DIR}/sharpen_ratios
 
-for RATIO in ${RATIOS[@]};
-do
-    echo 'Ratio '${RATIO}
-    python3 ${SCRIPTS}/sharpen_sh_parallel.py \
-            --in ${ODF_DIR}/csa.nii.gz \
-            --out ${ODF_DIR}/sharpen_ratios/csa_sharp_r${RATIO}.nii.gz \
-            --mask ${DIFF_DATA_DIR}/mask.nii.gz \
-            --ratio ${RATIO} \
-            --tau 0.1 --lambda 1. --csa_norm True \
-            --cores ${N_CORES}
+
+# RATIOS is a vriable,
+# why isnt SHMAX, REL_TH and ANGSEP variable too
+
+
+# write LSD config file
+echo "# Path to preprocessed DWI data" > ${LSD_DIR}'/config.txt'
+echo "DWI_PATH="${DIFF_DATA_RELEASE_DIR}'/data.nii.gz' >> ${LSD_DIR}'/config.txt'
+echo "# Path to b-values file" >> ${LSD_DIR}'/config.txt'
+echo "BVAL_PATH="${LSD_DIR}'/rounded.bval' >> ${LSD_DIR}'/config.txt'
+echo "# Path to b-vector file" >> ${LSD_DIR}'/config.txt'
+echo "BVEC_PATH="${LSD_DIR}'/flipX.bvec' >> ${LSD_DIR}'/config.txt'
+echo "# Path to brain mask" >> ${LSD_DIR}'/config.txt'
+echo "MASK_PATH="${DIFF_DATA_RELEASE_DIR}'/mask.nii.gz' >> ${LSD_DIR}'/config.txt'
+echo "# Path to noise sigmas" >> ${LSD_DIR}'/config.txt'
+echo "SIGMA_PATH="${NOISEMAP_DIR}'/sigmas.nii.gz' >> ${LSD_DIR}'/config.txt'
+echo "# Path to noise Ns" >> ${LSD_DIR}'/config.txt'
+echo "N_PATH="${NOISEMAP_DIR}'/Ns.nii.gz' >> ${LSD_DIR}'/config.txt'
+echo "# Basename for outputs" >> ${LSD_DIR}'/config.txt'
+echo "OUTPUT_BASE="${LSD_DIR}'/output' >> ${LSD_DIR}'/config.txt'
+echo "# List of deconvolution ratios" >> ${LSD_DIR}'/config.txt'
+RR='('
+for t in ${RATIOS[@]}; do
+  RR+=$t' '
 done
-
-
-echo 'Extracting Peaks'
-mkdir -p ${ODF_DIR}/peaks_ratios
-
-for RATIO in ${RATIOS[@]};
-do
-    echo 'Extracting peaks from sharpened Ratio '${RATIO}
-    python3 ${SCRIPTS}/peak_extraction.py \
-            ${ODF_DIR}/sharpen_ratios/csa_sharp_r${RATIO}.nii.gz \
-            ${ODF_DIR}/peaks_ratios/nufo_csa_sharp_r${RATIO}.nii.gz \
-            ${ODF_DIR}/peaks_ratios/dir_csa_sharp_r${RATIO}.nii.gz \
-            ${ODF_DIR}/peaks_ratios/len_csa_sharp_r${RATIO}.nii.gz \
-            --relth 0.25 --minsep 25 --maxn 10 \
-            --mask ${DIFF_DATA_DIR}/mask.nii.gz
-
-done
-
-
-echo 'Computing AIC for all peaks approximation'
-mkdir -p ${ODF_DIR}/aic_ratios
-
-for RATIO in ${RATIOS[@]};
-do
-    echo 'AIC from sharpened Ratio '${RATIO}
-    python3 ${SCRIPTS}/compute_aic_all_peaks.py \
-            --data ${DIFF_DATA_NORM_RELEASE_DIR}/data_norm.nii.gz \
-            --bval ${DIFF_DATA_NORM_RELEASE_DIR}/data_norm.bval \
-            --bvec ${DIFF_DATA_NORM_RELEASE_DIR}/data_norm.bvec \
-            --mask ${DIFF_DATA_DIR}/mask.nii.gz \
-            --inufo ${ODF_DIR}/peaks_ratios/nufo_csa_sharp_r${RATIO}.nii.gz \
-            --idirs ${ODF_DIR}/peaks_ratios/dir_csa_sharp_r${RATIO}.nii.gz \
-            --ilen ${ODF_DIR}/peaks_ratios/len_csa_sharp_r${RATIO}.nii.gz \
-            --sigma ${NOISEMAP_DIR}/sigma_norm.nii.gz \
-            --ratio ${RATIO} \
-            --oaic ${ODF_DIR}/aic_ratios/aic_csa_sharp_r${RATIO}.nii.gz \
-            --cores ${N_CORES}
-done
-
-
-# stack odf and aic filename in a list, in order of increasing ratios
-declare -a ODFFILELIST
-declare -a AICFILELIST
-for RATIO in ${RATIOS[@]};
-do
-    TMPODFFILE=${ODF_DIR}/sharpen_ratios/csa_sharp_r${RATIO}.nii.gz;
-    ODFFILELIST[${#ODFFILELIST[@]}+1]=$TMPODFFILE;
-    TMPAICFILE=${ODF_DIR}/aic_ratios/aic_csa_sharp_r${RATIO}.nii.gz;
-    AICFILELIST[${#AICFILELIST[@]}+1]=$TMPAICFILE;
-done
-
-
-# # Picks the ratio with lowest AIC for each voxel
-# python3 ${SCRIPTS}/combine_aic.py \
-#         --iaic ${AICFILELIST[@]} \
-#         --iodf ${ODFFILELIST[@]} \
-#         --mask ${DIFF_DATA_DIR}/mask.nii.gz \
-#         --ratios ${RATIOS[@]} \
-#         --oodf ${ODF_DIR}/best_voxelwise_aic_odf.nii.gz \
-#         --oaic ${ODF_DIR}/aic_ratios/best_voxelwise_aic_aic.nii.gz \
-#         --oratio ${ODF_DIR}/best_voxelwise_aic_ratio.nii.gz
+RR+=')'
+# echo "RATIOS=(1.1 1.2 1.4 1.6 1.8 2.0 2.2 2.4 2.6 2.8 3.0 3.5 4.0 4.5 5.0 5.5 6.0 7.0 8.0 10.0)" >> ${LSD_DIR}'/config.txt'
+echo "RATIOS="${RR} >> ${LSD_DIR}'/config.txt'
+echo "# Maximum SH order" >> ${LSD_DIR}'/config.txt'
+echo "SHMAX=8" >> ${LSD_DIR}'/config.txt'
+echo "# Relative amplitude threshold for peak extraction" >> ${LSD_DIR}'/config.txt'
+echo "PEAK_REL_TH=0.25" >> ${LSD_DIR}'/config.txt'
+echo "# Minimum separation (in degrees) for peak extraction" >> ${LSD_DIR}'/config.txt'
+echo "PEAK_ANG_SEP=25" >> ${LSD_DIR}'/config.txt'
+echo "# Patch size for AIC neighborhood (1 to turn OFF)  " >> ${LSD_DIR}'/config.txt'
+echo "PATCHSIZE=3" >> ${LSD_DIR}'/config.txt'
+echo "# Number of threads for multiprocessing (-1 for all available CPUs)" >> ${LSD_DIR}'/config.txt'
+echo "NCORE="${N_CORES} >> ${LSD_DIR}'/config.txt'
+echo " " >> ${LSD_DIR}'/config.txt'
 
 
 
-# Picks the ratio with lowest AIC for each voxel in neighborhood
-python3 ${SCRIPTS}/combine_aic_neigh.py \
-        --iaic ${AICFILELIST[@]} \
-        --iodf ${ODFFILELIST[@]} \
-        --mask ${DIFF_DATA_DIR}/mask.nii.gz \
-        --ratios ${RATIOS[@]} \
-        --oodf ${ODF_DIR}/odf_best_neighborhood_aic.nii.gz \
-        --oaic ${ODF_DIR}/aic_neighborhood_aic.nii.gz \
-        --oratio ${ODF_DIR}/ratio_best_neighborhood_aic.nii.gz
+# need to launch LSD script from the folder because paths arent setup properly
+cd /data/pt_02015/220601_LSD_Paper/lsd/
 
 
-echo 'Extracting peaks from best AIC ODFs'
-python3 ${SCRIPTS}/peak_extraction.py \
-        ${ODF_DIR}/odf_best_neighborhood_aic.nii.gz \
-        ${ODF_DIR}/nufo_best_neighborhood_aic.nii.gz \
-        ${ODF_DIR}/dir_best_neighborhood_aic.nii.gz \
-        ${ODF_DIR}/len_best_neighborhood_aic.nii.gz \
-        --relth 0.25 --minsep 25 --maxn 10 \
-        --mask ${DIFF_DATA_DIR}/mask.nii.gz
+sh dummy_LSD_script.sh ${LSD_DIR}'/config.txt'
 
 
-echo 'Normalize ODF'
-python3 ${SCRIPTS}/sh_odf_normalize.py \
-        ${ODF_DIR}/odf_best_neighborhood_aic.nii.gz \
-        ${ODF_DIR}/odf_norm_best_neighborhood_aic.nii.gz
+cd ${DIFF_DIR}
+
+
+
+
 
 
