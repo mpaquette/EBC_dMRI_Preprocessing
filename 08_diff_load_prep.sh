94b5d0e4f23ac5e1547f40bbf39b509aaee0aa05
Michael Paquette <jotha885@gmail.com>
2024-02-05 12:18:37 +0100

    wip

--- 08_diff_load_prep.sh
+++ 08_diff_load_prep.sh
@@ -333,6 +333,8 @@ while [ $MASKING_DONE == 0 ]; do
              -odt int
 
     # Check the results
+    echo "mrview -load ${IM_IN}-interpolation 0 -mode 2 -overlay.load ${DIFF_DATA_DIR}/mask_tmp_connect_dil.nii.gz -overlay.opacity 0.5 -overlay.interpolation 0 -overlay.colourmap 3"
+
     mrview \
             -load ${IM_IN} \
             -interpolation 0  \
